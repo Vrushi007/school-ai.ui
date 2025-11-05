d0653aaf120b22e33a00f78d00b7cb7c9c15b5dc
Vrushabh Rajaghatta <vrishabhrajaghatta@gmail.com>
2025-11-05 15:03:13 +0000

    Changes to deploy outside gh-pages

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,8 +1,8 @@
 export const ROUTES = {
-  HOME: "/school-ai.ui/home",
-  TEACHER_DASHBOARD: "/school-ai.ui/teacher",
-  STUDENT_DASHBOARD: "/school-ai.ui/student",
-  LESSON_PLANNER: "/school-ai.ui/teacher/lesson-plan",
-  QUESTION_PAPER_GENERATOR: "/school-ai.ui/teacher/question-paper",
-  STUDENT_GET_ANSWERS: "/school-ai.ui/student/get-answers",
+  HOME: "",
+  TEACHER_DASHBOARD: "/teacher",
+  STUDENT_DASHBOARD: "/student",
+  LESSON_PLANNER: "/teacher/lesson-plan",
+  QUESTION_PAPER_GENERATOR: "/teacher/question-paper",
+  STUDENT_GET_ANSWERS: "/student/get-answers",
 };
